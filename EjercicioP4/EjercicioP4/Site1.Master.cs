cefa1a1f757206e765cb6b612f6ec170a9461516
DiegoHuamanR <72468973@ucsm.edu.pe>
2021-09-15 08:35:44 -0500

    Ejercicio de la Práctica 4.

---
+++ EjercicioP4/EjercicioP4/Site1.Master.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjercicioP4
+{
+
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        String seleccionado;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListItem microondas = new ListItem("Microondas", "20");
+                objetos.Items.Add(microondas);
+            }
+        }
+
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            seleccionado = objetos.SelectedItem.ToString();
+            if (seleccionado == "Lavadora")
+            {
+     
+                grafico.Src = "" + objetos.SelectedItem.ToString() + ".jpg";
+                grafico.Visible = true;
+                codigo.Text = "15632";
+                nombre.Text = "Lavadora";
+                descripcion.Text = "Es un equipo eléctrico que facilita el lavado de la ropa";
+                marca.Text = "LG";
+                precio.Text = "S/.899";
+
+
+            }
+            if (seleccionado == "Microondas")
+            {
+                s1.Text = seleccionado;
+                grafico.Src = "" + objetos.SelectedItem.ToString() + ".jpg";
+                grafico.Visible = true;
+                codigo.Text = "15633";
+                nombre.Text = "Microondas";
+                descripcion.Text = "Aparato eléctrico que permite la cocción o el calentamiento de alimentos";
+                marca.Text = "Samsung";
+                precio.Text = "S/.489";
+            }
+            if (seleccionado == "Cocina")
+            {
+                s1.Text = seleccionado;
+                grafico.Src = "" + objetos.SelectedItem.ToString() + ".jpg";
+                grafico.Visible = true;
+                codigo.Text = "15634";
+                nombre.Text = "Cocina";
+                descripcion.Text = "Aparato eléctrico que permite calentar los alimentos";
+                marca.Text = "LG";
+                precio.Text = "S/.1799";
+
+            }
+            if (seleccionado == "Plancha")
+            {
+                s1.Text = seleccionado;
+                grafico.Src = "" + objetos.SelectedItem.ToString() + ".jpg";
+                grafico.Visible = true;
+                codigo.Text = "15634";
+                nombre.Text = "Cocina";
+                descripcion.Text = "Electrodomestico que sirve para alisar la ropa";
+                marca.Text = "Philips";
+                precio.Text = "S/.146";
+            }
+        }
+    }
+    }
---
+++ EjercicioP4/EjercicioP4/Site1.Master.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EjercicioP4
+{
+
+
+    public partial class Site1
+    {
+
+        /// <summary>
+        /// Control head.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+
+        /// <summary>
+        /// Control titulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label titulo;
+
+        /// <summary>
+        /// Control objetos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList objetos;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Control s1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label s1;
+
+        /// <summary>
+        /// Control datos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTable datos;
+
+        /// <summary>
+        /// Control codigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label codigo;
+
+        /// <summary>
+        /// Control nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label nombre;
+
+        /// <summary>
+        /// Control descripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label descripcion;
+
+        /// <summary>
+        /// Control marca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label marca;
+
+        /// <summary>
+        /// Control precio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label precio;
+
+        /// <summary>
+        /// Control grafico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage grafico;
+    }
+}
